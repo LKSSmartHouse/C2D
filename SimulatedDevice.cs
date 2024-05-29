c3e8648b53b8afda0e82b77346adc0c5b4dff58b
LKS <iker.arrien@opendeusto.es>
2024-05-29 10:48:03 +0200

    Twin correctly updated

--- SimulatedDevice.cs
+++ SimulatedDevice.cs
@@ -42,54 +42,46 @@ namespace simulatedDevice
             {
                 registryManager = RegistryManager.CreateFromConnectionString("HostName=iothub-deusto.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=b9UWI1NdpA0lvAV6NMJ44G25pGzAOJnGNAIoTKukjgg=");
                 Twin twin = await registryManager.GetTwinAsync("smartmeter1");
-                var jsonContent = twin.ToJson();
 
                 ClientMessage receivedMessage = await s_deviceClient.ReceiveAsync();
                 if (receivedMessage == null) continue;
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 String message = Encoding.ASCII.GetString(receivedMessage.GetBytes());
                 Console.WriteLine("Received message: {0}", message);
-                List<String> desiredProperties = ReadDesiredProperties(message);
-                UpdateTwin(jsonContent, desiredProperties);
+                List<String> desiredProperties = ReadDesiredProperties(message, twin);
+                string jsonPatch = $@"{{
+                    'properties': {{
+                        'desired': {{
+                            'frecuency': '{desiredProperties[1]}',
+                            'reducir_consumo': '{desiredProperties[2]}',
+                        }}
+                    }}
+                }}";
+                await registryManager.UpdateTwinAsync(twin.DeviceId, jsonPatch, twin.ETag);
+                Console.WriteLine("json actualizado");
                 Console.ResetColor();
 
                 await s_deviceClient.CompleteAsync(receivedMessage);
             }
         }
-        private static List<String> ReadDesiredProperties(String message) {
+        private static List<String> ReadDesiredProperties(String message, Twin twin) {
             List<String> properties = new List<String>();
             String[] datas = message.Split(",");
             foreach (String s in datas) {
-                properties.Add(s);
+                String[] datas2 = s.Split(":");
+                if (datas2[0] == "frecuency") {
+                    properties.Add(datas2[1]);
+                } else {
+                    properties.Add(twin.Properties.Desired["frecuency"]);
+                }
+                if (datas2[0] == "reducir_consumo") {
+                    properties.Add(datas2[1]);
+                } else {
+                    properties.Add(twin.Properties.Desired["reducir_consumo"]);
+                }
             }
+
             return properties;
-        }
-        
-        static void UpdateTwin(string jsonContent, List<string> desiredProperties) {
-            JObject jsonObject = JObject.Parse(jsonContent);
-            Console.WriteLine(jsonObject.ToString());
-            dynamic jsonObj = JsonConvert.DeserializeObject(jsonObject.ToString());
-            jsonObj["properties"]["desired"]["frecuency"] = desiredProperties[0];
-            jsonObj["properties"]["desired"]["reducir_consumo"] = desiredProperties[1];
-            TwinCollection updatedTwin = new TwinCollection(jsonObj.ToString());
-            Console.WriteLine(updatedTwin.ToString());
-            string jsonPatch = $@"{{
-                    'properties': {{
-                        'desired': {{
-                            'Frequency': '{desiredProperties[0]}',
-                            'reducir_consumo': '{desiredProperties[1]}'
-                            }}
-                        }}
-                    }}
-                }}";
-            Twin twin = registryManager.GetTwinAsync("smartmeter1");
-            TwinCollection desiredProperties1 = new TwinCollection(jsonPatch);
-
-            twin.Properties.Desired = desiredProperties1;
-
-            // Actualizar el gemelo
-            twin = registryManager.UpdateTwinAsync(jsonObj["deviceId"], twin, twin.ETag);
-
         }
 
         private static void Main()
